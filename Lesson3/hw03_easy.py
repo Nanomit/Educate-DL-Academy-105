789b503138428ac8941737d57ddc73c76ea5245d
Konstantin <filinworking@gmail.com>
2021-02-10 16:11:31 +0400

    Lesson 3 v0.1

--- Lesson3/hw03_easy.py
+++ Lesson3/hw03_easy.py
@@ -1,3 +1,4 @@
+import random
 # Задача-1:
 # Дан список фруктов.
 # Напишите программу, выводящую фрукты в виде нумерованного списка,
@@ -13,13 +14,42 @@
 
 # Подсказка: воспользоваться методом .format()
 
+print('Start program 1')
+
+fruit = ["Яблоко", "Банан", "Киви", "Арбуз"]
+for i, fruit in enumerate(fruit):
+    i = i + 1
+    print('{}. {:>6}'.format(i, fruit))
+
+
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
+
+print("\nStart program 2")
+
+lst1 = [random.randrange(1, 20, 1) for i in range(10)]
+lst2 = [random.randrange(1, 20, 1) for i in range(10)]
+print('Список 1 = {} \nСписок 2 = {}'.format(lst1, lst2))
+for i in lst2:
+    if i in lst1:
+        lst1.remove(i)
+print('Список =', lst1)
 
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+print("\nStart program 3")
+
+lst1 = [random.randrange(1, 20, 1) for i in range(5)]
+print('Список =', lst1)
+lst2 = []
+for i in lst1:
+    if i % 2 == 0:
+        lst2.append(i / 4)
+    else:
+        lst2.append(i * 2)
+print(lst2)
